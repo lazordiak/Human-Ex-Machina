92c5be0fe8a4c9ef8a899e4cc4d4f0df6dca305f
Schuyler deVos <schuylerdevos@gmail.com>
2020-11-17 10:09:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Human Ex Machina
+ Album site
---
+++ data.js
@@ -0,0 +1,189 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano1",
+      "name": "Turing Machine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.4047610158291235,
+        "pitch": 0.5737410675140957,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.782980007147298,
+          "pitch": 0.0880622794858148,
+          "rotation": 6.283185307179586,
+          "target": "1-pano2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5553962792647376,
+          "pitch": -0.7048014981192594,
+          "title": `When you hear the word "Artificial Intelligence" what do YOU, personally, think of? Please describe.`,
+          "text": "Computer, machine learning, engineering, future, robots, consciousness, hope"
+        },
+        {
+          "yaw": -2.786422357666062,
+          "pitch": 0.01621026977148432,
+          "title": `When you hear the word "Artificial Intelligence" what do YOU, personally, think of? Please describe.`,
+          "text": "Terminator and similar dystopia sci-fi movies. Basically AI taking over and killing everyone."
+        },
+        {
+          "yaw": -1.786422357666062,
+          "pitch": -0.01621026977148432,
+          "title": `When you hear the word "Artificial Intelligence" what do YOU, personally, think of? Please describe.`,
+          "text": "Terminator and similar dystopia sci-fi movies. Basically AI taking over and killing everyone."
+        },
+        {
+          "yaw": 5.786422357666062,
+          "pitch": 0.21621026977148432,
+          "title": `When you hear the word "Artificial Intelligence" what do YOU, personally, think of? Please describe.`,
+          "text": "Terminator and similar dystopia sci-fi movies. Basically AI taking over and killing everyone."
+        }
+      ]
+    },
+    {
+      "id": "1-pano2",
+      "name": "Cyborg Baby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.2780530643511092,
+        "pitch": -0.08674500922718842,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5715098456641474,
+          "pitch": 0.06736579903137496,
+          "rotation": 0,
+          "target": "2-pano3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1217804597721841,
+          "pitch": -0.14674140235731414,
+          "title": "Drink",
+          "text": "AND laundromat"
+        },
+        {
+          "yaw": -1.2911509702659885,
+          "pitch": -0.23734823522592663,
+          "title": "\"Wish I had in unit laundry...\"",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-pano3",
+      "name": "Schuyler's Song",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.20455563642277141,
+        "pitch": -0.27093690163576234,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9016760709295415,
+          "pitch": 0.10492025898410873,
+          "rotation": 0,
+          "target": "3-pano4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22917476348097843,
+          "pitch": -0.7112181103032285,
+          "title": "Sup",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-pano4",
+      "name": "Age of Adz by Sufjan Stevens",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.25377777101533994,
+        "pitch": -0.32291246304910715,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.004188581044799,
+          "pitch": 0.2957307363597579,
+          "rotation": 0,
+          "target": "0-pano1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.049191709500858494,
+          "pitch": -0.7624099654867322,
+          "title": "We can fuck,",
+          "text": "but the mask STAYS ON"
+        },
+        {
+          "yaw": 1.9743784537325348,
+          "pitch": -0.13503563339452285,
+          "title": "Time is cyclical",
+          "text": "Let's go back to the beginning"
+        }
+      ]
+    }
+  ],
+  "name": "Eddy",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
